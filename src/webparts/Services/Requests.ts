5737a0db7182adfadc294b50b68f8ee99f7f5784
Maya-Mostafa <mayamostafa@gmail.com>
2022-06-24 12:21:50 -0400

    adding final features

---
+++ src/webparts/Services/Requests.ts
@@ -0,0 +1,92 @@
+
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { SPPermission } from "@microsoft/sp-page-context";
+import {SPHttpClient, ISPHttpClientOptions} from "@microsoft/sp-http";
+
+export const getPgAttachments = async (context: WebPartContext) =>{
+
+    console.log("context", context)
+
+    const pageUrl = document.documentURI;
+    const pageTitle = pageUrl.substring(pageUrl.lastIndexOf('/')+1, pageUrl.lastIndexOf('.aspx'));
+
+    const responseUrl = `${context.pageContext.web.absoluteUrl}/_api/web/GetFolderByServerRelativeUrl('siteassets/sitepages/${pageTitle}')/files`;
+    const response = await context.spHttpClient.get(responseUrl, SPHttpClient.configurations.v1).then(r => r.json());
+    return response;
+};
+
+export const getFileIcon = (fileUrl: String) =>{
+    let fileIconUrl = '';
+    if (fileUrl.indexOf('.docx') !== -1 || fileUrl.indexOf('.doc') !== -1){
+        fileIconUrl =  "https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/docx.svg";    
+    }
+    else if (fileUrl.indexOf('.pdf') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/pdf.svg';
+    }
+    else if (fileUrl.indexOf('.xls') !== -1 || fileUrl.indexOf('.xlsx') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/xlsx.svg';
+    }
+    else if (fileUrl.indexOf('.css') !== -1 || fileUrl.indexOf('.js') !== -1 || fileUrl.indexOf('.html') !== -1 || fileUrl.indexOf('.htm') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/code.svg';
+    }
+    else if (fileUrl.indexOf('.ppt') !== -1 || fileUrl.indexOf('.pptx') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/pptx.svg';
+    }
+    else if (fileUrl.indexOf('.vsd') !== -1 || fileUrl.indexOf('.vsdx') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/vsdx.svg';
+    }
+    else if (fileUrl.indexOf('onrnote') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/onetoc.svg';
+    }
+    else if (fileUrl.indexOf('.url') !== -1){
+        fileIconUrl = 'https://modernb.akamai.odsp.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/link.svg';
+    }
+    else if (fileUrl.indexOf('.png') || fileUrl.indexOf('.jpg') || fileUrl.indexOf('.bmp') || fileUrl.indexOf('.gif')){
+        fileIconUrl = 'https://res-1.cdn.office.net/files/fabric-cdn-prod_20220127.003/assets/item-types/20/photo.svg';
+    }
+
+    return fileIconUrl;
+};
+
+
+export const getOpenInBrowserLink = (context: WebPartContext, obj: any) => {
+    const officeExts = ['.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx', '.vsd', '.vsdx'];
+    const fileExt = obj.ServerRelativeUrl.substring(obj.ServerRelativeUrl.indexOf('.'));
+    let fileUrl = obj.ServerRelativeUrl;
+
+    if (officeExts.indexOf(fileExt) !== -1){
+        fileUrl = `${context.pageContext.web.absoluteUrl}/_layouts/15/Doc.aspx?sourcedoc=%7B${obj.UniqueId}%7D&file=${obj.Name}&action=default&mobileredirect=true`;
+    }
+    else {
+        fileUrl = `${context.pageContext.web.absoluteUrl}/SiteAssets/Forms/AllItems.aspx?id=${obj.ServerRelativeUrl}&parent=${obj.ServerRelativeUrl.substring(0, obj.ServerRelativeUrl.indexOf(obj.Name)-1)}`;
+    }
+
+    return fileUrl;
+};
+
+export const deleteAttachment = async (context: WebPartContext, item: any) => {
+
+    const restUrl = `${context.pageContext.web.absoluteUrl}/_api/web/GetFileByServerRelativePath(decodedurl='${item.ServerRelativeUrl}')/recycle`;
+
+    let spOptions: ISPHttpClientOptions = {
+        headers:{
+            Accept: "application/json;odata=nometadata", 
+            "Content-Type": "application/json;odata=nometadata",
+            "odata-version": "",
+            "IF-MATCH": "*",
+            // "X-HTTP-Method": "DELETE"         
+        },
+    };
+
+    const _data = await context.spHttpClient.post(restUrl, SPHttpClient.configurations.v1, spOptions);
+    if (_data.ok){
+        console.log('Attachment is deleted!');
+    }
+};
+
+export const isUserManage = (context: WebPartContext) : boolean =>{
+    const userPermissions = context.pageContext.web.permissions,
+        permission = new SPPermission (userPermissions.value);
+    
+    return permission.hasPermission(SPPermission.manageWeb);
+};
--- src/webparts/attachments/components/Attachments.tsx
+++ src/webparts/attachments/components/Attachments.tsx
@@ -1,31 +1,84 @@
 import * as React from 'react';
 import styles from './Attachments.module.scss';
 import { IAttachmentsProps } from './IAttachmentsProps';
-
-import { ListItemAttachments } from '@pnp/spfx-controls-react/lib/ListItemAttachments';
-import { getControlledDerivedProps } from 'office-ui-fabric-react/lib/Foundation';
-import {SPHttpClient} from "@microsoft/sp-http";
-
+import {getPgAttachments, getFileIcon, getOpenInBrowserLink, deleteAttachment, isUserManage} from '../../Services/Requests';
+import {Icon, Dialog, DialogFooter, PrimaryButton, DefaultButton, Spinner, SpinnerSize, Overlay, DialogType} from 'office-ui-fabric-react';
+import { useBoolean } from '@uifabric/react-hooks';
 
 export default function Attachments (props: IAttachmentsProps){
 
+  const [attachments, setAttachments] = React.useState([]);
+  const [attachItem, setAttachItem] = React.useState(null);
+  const [isDataLoading, { toggle: toggleIsDataLoading }] = useBoolean(false);
+  const [hideDeleteDialog, {toggle: toggleHideDeleteDialog}] = useBoolean(true);
+
+  const deleteDialogContentProps = {
+    type: DialogType.close,
+    title: "Delete File"
+  }; 
+
   React.useEffect(()=>{
-    console.log(props.context.pageContext.listItem.id);
-
-    const responseUrl = `${props.context.pageContext.web.absoluteUrl}/_api/web/Lists/GetByTitle('Site Pages')/items(33)'`;
-    const response = props.context.spHttpClient.get(responseUrl, SPHttpClient.configurations.v1).then(r => r.json());
-    response.then(result => console.log(result['Description']));
-
+    getPgAttachments(props.context).then(r=> {console.log(r.value);setAttachments(r.value);});
   }, []);
 
+  const onDeleteClickHandler = (item: any) => {
+    setAttachItem(item);
+    toggleHideDeleteDialog();
+  };
+  const onDeleteAttachment = () => {
+    toggleHideDeleteDialog();
+    toggleIsDataLoading();
+    deleteAttachment(props.context, attachItem).then(r1 => {
+      getPgAttachments(props.context).then(r=> {
+        setAttachments(r.value);
+        toggleIsDataLoading();
+      });      
+    });
+  };
 
   return (
     <div className={ styles.attachments }>
-      Test Attachments...
-      {/* <ListItemAttachments listId='d021c002-c529-4a37-8a06-e0e16276cd2f'
-        context={this.props.context}
-        disabled={false} 
-      /> */}
+      <ul className={styles.attachmentsList}>
+        {attachments.map(item => {
+          return (
+            <li>
+              <a className={styles.attachmentLinkOpen} href={getOpenInBrowserLink(props.context, item)} target="_blank" data-interception="off">
+                <i>
+                  <img width="20px" src={getFileIcon(item.ServerRelativeUrl)} alt={item.Name}/>
+                </i>
+                <span>{item.Name.substring(0,item.Name.lastIndexOf('.'))}</span>
+              </a>
+              <a className={styles.attachmentLinkDownload} href={`${item.ServerRelativeUrl}`} title='Download' download>
+                <Icon iconName='Download' />
+              </a>
+              {isUserManage &&
+                <a className={styles.attachmentDelete} onClick={() => onDeleteClickHandler(item)} title='Delete'>
+                  <Icon iconName='Delete' />
+                </a>
+              }
+            </li>
+          );
+        })}
+      </ul>
+      <Dialog
+            hidden={hideDeleteDialog}
+            onDismiss={toggleHideDeleteDialog} isBlocking={true}
+            dialogContentProps={deleteDialogContentProps}>
+            <p>Are you sure you want to delete this file? </p>
+            {isDataLoading &&
+              <>
+                  <Overlay></Overlay>
+                  <div>
+                      <Spinner size={SpinnerSize.medium} label="Please Wait, Updating Attachments..." ariaLive="assertive" labelPosition="right" />
+                  </div>
+              </>
+            }
+            <DialogFooter>
+                <PrimaryButton onClick={onDeleteAttachment} text="Yes" />
+                <DefaultButton onClick={toggleHideDeleteDialog} text="No" />
+            </DialogFooter>
+        </Dialog>
+        
     </div>
   );
 }
